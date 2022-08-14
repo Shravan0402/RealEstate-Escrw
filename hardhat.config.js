c1447b46175a4c2859e895f46bcf8db7e7de369e
Arman Aurobindo <arman8895884469@.com>
2022-08-15 01:04:54 +0530

    Deployment script added

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require('dotenv').config()
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.8",
+  networks:{
+    mumbai : {
+      url:process.env.ALCHEMY_MUMBAI_URL,
+      accounts:[process.env.PRIVATE_KEY],
+      chainId : 80001
+    }
+  }
 };
